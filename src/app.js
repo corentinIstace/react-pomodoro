88b10e78dd88903f271f35fb86fc2937374e60cd
corentinIstace <corentinistace1@gmail.com>
2021-10-18 14:29:02 +0200

    fix the problem with eslint

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
-/* eslint-disable react/jsx-no-literals */
 import React from "react";
 function app() {
     return (
         <>
-            <h1>bonjour</h1>
+            <h1>{"bonjour"}</h1>
+            <button type={"button"}>{"-"}</button>
+            <button type={"button"}>{"start"}</button>
+            <button type={"button"}>{"breack"}</button>
+            <button type={"button"}>{"restart"}</button>
+            <button type={"button"}>{"reset"}</button>
+            <button type={"button"}>{"+"}</button>
         </>
     );
 }
