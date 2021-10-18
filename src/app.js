496e7de0a581aadade9b3d9cd32c461c6282b588
corentinIstace <corentinistace1@gmail.com>
2021-10-18 13:38:10 +0200

    adding eslint and prettier, try to resolve problem with babel, fix in vs code for the moment

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-function App() {
-    
+/* eslint-disable react/jsx-no-literals */
+import React from "react";
+function app() {
     return (
         <>
-        <h1>Hello world!</h1>
+            <h1>bonjour</h1>
         </>
-    )
+    );
 }
-
-export default App;
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from './app.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./app.js";
 
 ReactDOM.render(
     <React.StrictMode>
-      <App />
+        <App />
     </React.StrictMode>,
-    document.getElementById('root')
-  );
+    document.querySelector("#root"),
+);
