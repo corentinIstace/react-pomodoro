6a3c3e2e16777d15dc00aecf068113febba6223a
corentinIstace <corentinistace1@gmail.com>
2021-10-18 16:50:46 +0200

    display buttons and adding fontawsome

--- src/app.js
+++ src/app.js
@@ -2,13 +2,33 @@ import React from "react";
 function app() {
     return (
         <>
-            <h1>{"bonjour"}</h1>
-            <button type={"button"}>{"-"}</button>
-            <button type={"button"}>{"start"}</button>
-            <button type={"button"}>{"breack"}</button>
-            <button type={"button"}>{"restart"}</button>
-            <button type={"button"}>{"reset"}</button>
-            <button type={"button"}>{"+"}</button>
+            <div id={"timer_container"}>
+                <h1>{"timer"}</h1>
+            </div>
+            <div id={"buttons_container"}>
+                <button type={"button"} id={"less_button"}>
+                    <i className={"far fa-minus-square"} />
+                </button>
+                <button type={"button"} id={"start_button"}>
+                    {"start"}
+                    <i className={"far fa-play-circle"} />
+                </button>
+                <button type={"button"} id={"breack_button"}>
+                    {"breack"}
+                    <i className={"far fa-pause-circle"} />
+                </button>
+                <button type={"button"} id={"restart_button"}>
+                    {"restart"}
+                    <i className={"fas fa-undo-alt"} />
+                </button>
+                <button type={"button"} id={"reset_button"}>
+                    {"reset"}
+                    <i className={"far fa-stop-circle"} />
+                </button>
+                <button type={"button"} id={"more_button"}>
+                    <i className={"far fa-plus-square"} />
+                </button>
+            </div>
         </>
     );
 }
